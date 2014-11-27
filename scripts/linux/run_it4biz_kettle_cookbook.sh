2577c59152024743f67cf6227b0062431e3bb945
Caio Moreno de Souza <caiomsouza@gmail.com>
2014-11-27 16:18:32 -0200

    melhoria no run_it4biz_kettle_cookbook.sh

--- scripts/linux/run_it4biz_kettle_cookbook.sh
+++ scripts/linux/run_it4biz_kettle_cookbook.sh
@@ -2,4 +2,4 @@
 
 cd /Applications/Pentaho/PDI/pdi-ce-5.2.0.0-209/data-integration
 
-sh kitchen.sh -file:/Users/caiomsouza/svn/pentaho.community.projects/kettle-cookbook/trunk/pdi/document-folder.kjb -param:"INPUT_DIR"=/Users/caiomsouza/git/Bitbucket/curso-etl-sp-nov14/etls_source/ -param:"OUTPUT_DIR"=/Users/caiomsouza/git/Bitbucket/curso-etl-sp-nov14/it4biz-kettle-cookbook/ -param:"SAXON"=./lib/saxon-9.1.0.8.jar
+sh kitchen.sh -file:/Users/caiomsouza/git/github.com/it4biz-kettle-cookbook/pdi/document-folder.kjb -param:"INPUT_DIR"=/Users/caiomsouza/git/Bitbucket/curso-etl-sp-nov14/etls_source/ -param:"OUTPUT_DIR"=/Users/caiomsouza/git/Bitbucket/curso-etl-sp-nov14/it4biz-kettle-cookbook/ -param:"SAXON"=./lib/saxon-9.1.0.8.jar
